cfc61cb18253e6db5e350f623c33bf89f7c8e20b
Keks <keks@htmlacademy.ru>
2023-06-17 12:05:28 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/main.js
+++ 4/js/main.js
@@ -6,7 +6,9 @@ const MIN_NUMBER_FOR_COMMENT = 0;
 const MAX_NUMBER_FOR_COMMENT = 30;
 const MIN_NUMBERS_FOR_LIKES = 15;
 const MAX_NUMBERS_FOR_LIKES = 200;
-const MESSAGE = [
+
+//Массивы
+const messages = [
   'Всё отлично!',
   'В целом всё неплохо.Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.В конце концов это просто непрофессионально.',
@@ -14,7 +16,7 @@ const MESSAGE = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают.Как можно было поймать такой неудачный момент ? !',
 ];
-const AVATAR_NAMES = [
+const avatarNames = [
   'Лео Месси',
   'Неймар',
   'Карим Бензема',
@@ -26,7 +28,7 @@ const AVATAR_NAMES = [
   'Мартин Эдегор',
   'Винисиус Жуниор',
 ];
-const DESCRIPTION = [
+const description = [
   'Сегодня был хороший день!',
   'Уставшие, но довольные',
   'Записываемся к моему мастеру. Он творит чудеса!',
@@ -78,20 +80,28 @@ const createGeneratorInRange = (firstIndex, lastIndex) => {
 const generatePhotoId = createGeneratorInRange(MIN_NUMBER_FOR_FUNCTIONS, MAX_NUMBER_FOR_FUNCTIONS);
 const generateMessageId = createGeneratorInRange(MIN_NUMBER_FOR_FUNCTIONS, Infinity);
 
+//Генерируем сообщение
+const generateMessage = (fn) => {
+  const message = [];
+  while (message.length < fn) {
+    message.push(getRandomArrayElement(messages));
+  }
+  return message;
+};
 
 //Генерируем коммент
 const createComment = () => ({
   id: generateMessageId(),
   avatar: `img/avatar-${getRandomInteger(MIN_NUMBER_FOR_FUNCTIONS, MAX_NUMBER_FOR_AVATAR)}.svg`,
-  message: getRandomArrayElement(MESSAGE),
-  name: getRandomArrayElement(AVATAR_NAMES),
+  message: generateMessage(getRandomInteger(MIN_NUMBER_FOR_FUNCTIONS, 2)).join(' '),
+  name: getRandomArrayElement(avatarNames),
 });
 
 //Генерируем Фото
 const createPhoto = () => ({
   id: generatePhotoId(),
   url: `photos/${getRandomInteger(MIN_NUMBER_FOR_FUNCTIONS, MAX_NUMBER_FOR_FUNCTIONS)}.jpg`,
-  description: getRandomArrayElement(DESCRIPTION),
+  description: getRandomArrayElement(description),
   likes: getRandomInteger(MIN_NUMBERS_FOR_LIKES, MAX_NUMBERS_FOR_LIKES),
   comment: Array.from({ length: getRandomInteger(MIN_NUMBER_FOR_COMMENT, MAX_NUMBER_FOR_COMMENT) }, createComment),
 });
@@ -99,4 +109,4 @@ const createPhoto = () => ({
 const generatePhoto = Array.from({ length: MAX_NUMBER_FOR_FUNCTIONS }, createPhoto);
 
 // eslint-disable-next-line no-unused-expressions
-generatePhoto;
+generatePhoto();
